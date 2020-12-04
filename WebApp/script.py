30ac2e5a0d9518775895eb928b40b1b546f88996
J-sonLi <jason.li31@myhunter.cuny.edu>
2020-12-04 17:40:25 -0500

    Added test database connection

---
+++ WebApp/script.py
@@ -0,0 +1,14 @@
+import os
+DB_HOST = os.environ.get('DB_HOST')
+DB_NAME = os.environ.get('DB_NAME')
+DB_USER = os.environ.get('DB_USER')
+DB_PASS = os.environ.get('DB_PASS')
+import psycopg2
+
+db_connect = psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASS, host=DB_HOST)
+db_cur = db_connect.cursor()
+
+db_cur.execute("INSERT INTO vpusers VALUES(%s, %s ,%s ,%s)", (0, "Test Name", "TestName@email.com", "Test Name st"))
+db_connect.commit()
+db_cur.close()
+db_connect.close()
