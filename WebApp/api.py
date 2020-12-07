345d012c6be2a82ab5ea6ac25feb3cc9ab1f90d7
jam-spoon <jamespoon08@gmail.com>
2020-12-07 16:16:57 -0500

    Removed testing properties

--- WebApp/api.py
+++ WebApp/api.py
@@ -7,20 +7,21 @@ from flask import flash
 import logging
 # get a key here https://console.developers.google.com/apis/credentials
 key = os.environ.get('CIVICS_API_KEY')
-#electionId = 2000 for testing purposes
-# electionId = 2000 
+# electionId = 2000 for testing purposes
+# electionId = 2000
 # Google Civics VoterInfoQuery API to get pollsite location
 def poll_search(address):
     params = {
         'address': address,
         'key': key
-        #'electionId': electionId
+    #    'electionId': electionId
     }
     #address.replace('+','%20')
     # call api
     url = 'https://civicinfo.googleapis.com/civicinfo/v2/voterinfo?'
     req = requests.get(url, params=params)
     data = req.json()
+    polling_hours = ""
     #print (data)
     try:
         # GCivic poll location information
@@ -38,10 +39,12 @@ def poll_search(address):
         # location_zip2 = data['earlyVoteSites'][0]['address']['zip']
         # full_location2 = location_address2 + ', ' + location_city2 + ', ' + location_state2 + ', ' + location_zip2
         # polling hours
-        polling_hours = data['pollingLocations'][0]['pollingHours']
-        flash(location_name,"Location Name:")
-        flash(full_location,"Full Address:")
-        flash(polling_hours,"Polling Hours:")
+        try:
+            polling_hours = data['pollingLocations'][0]['pollingHours']
+        except:
+            flash(location_name,"Location Name:")
+            flash(full_location,"Full Address:")
+            flash(polling_hours,"Polling Hours:")
         # flash(full_location2)
         return full_location
     # Keyerror for polling location
